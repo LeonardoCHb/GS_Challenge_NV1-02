e137b35c6df09ddac05bc2e400a1a8135efe0b52
LeonardoCHb <leonardohably@hotmail.com>
2020-08-20 20:30:22 -0300

    adding feature

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import api from './services/api'
 
 import "./styles.css";
 
+
 function App() {
+  const [repositories,setRepositories] = useState([])
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+        setRepositories(response.data)
+        console.log(response.data)
+    })
+}, [])
   async function handleAddRepository() {
-    // TODO
+
+    const response = await api.post('repositories',{
+      title: `Novo projeto ${Date.now()}`,
+      url: "https://github.com/LeonardoCHb/Projeto_Pizzaria.git",
+      techs: [
+      Node.js,
+      React,
+      ],
+  })
+      const repository = response.data
+
+      setRepositories([...repositories,repository])
+
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    // TODO 
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
+      {repositories.map(repository => <li key={repository.id}>{repository.title}
+      
+        
           <button onClick={() => handleRemoveRepository(1)}>
             Remover
           </button>
-        </li>
+          </li>)}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
